37d90b8e4cce34c873c51d7d1c889ff176d30aab
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-08-07 23:09:56 +0530

    my second program using xor

--- arrays/missing_number_using_xor.cpp
+++ arrays/missing_number_using_xor.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 int main()
 {
-int n,xor1=0,xor2=0,ans,N;
-    cout<<"enter the size of the array:";
-    cin>>n;
-    n=N-1;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cout<<"enter the elements:"<<i+1<<endl;
-        cin>>arr[i];
+    int N, xor1 = 0, xor2 = 0, ans;
+    cout << "Enter the total number of elements (N): ";
+    cin >> N;
+    int arr[N - 1];
+    for(int i = 0; i < N - 1; i++) {
+        cout << "Enter element " << i + 1 << ": ";
+        cin >> arr[i];
     }
-    cout<<"the original array is :";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    cout << "The original array is: ";
+    for(int i = 0; i < N - 1; i++) {
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    for(int i=1;i<=N;i++){
-        xor1=xor1^i;
+    cout << endl;
+    for(int i = 1; i <= N; i++) {
+        xor1 ^= i;
     }
-    for(int i=0;i<N-1;i++){
-        xor2=xor2^arr[i];
+    for(int i = 0; i < N - 1; i++) {
+        xor2 ^= arr[i];
     }
-    ans=xor1^xor2;
-    cout<<"the missing number is:"<<ans;
+    ans = xor1 ^ xor2;
+    cout << "The missing number is: " << ans;
 }
