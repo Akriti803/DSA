1387327d9febdf51cf8687e41fcd247f433bfbe3
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-08-10 22:22:26 +0530

    found the max sum using brute force

---
+++ arrays/maximum_subarray_sum_brute_force.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int main()
+{
+    int n,sum=0,maximum_sum=INT_MIN;
+    cout<<"enter the size of the array:";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter the elements"<<i+1<<":"<<endl;
+        cin>>arr[i];
+    }
+    cout<<endl;
+    cout<<"the array is:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+            sum+=arr[end];
+            maximum_sum=max(sum,maximum_sum);  
+        }
+    }
+    cout<<"the maximum sum is:"<<maximum_sum;
+
+}
