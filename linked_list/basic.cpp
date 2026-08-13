4ee0e5f5641465acf6ea51d051990b215107b694
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-08-13 23:52:06 +0530

    did basic concept like push back and push front

---
+++ linked_list/basic.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+       int data;
+       Node* next;
+       Node(int val){
+        data =val;
+        next=NULL;
+       }
+    
+
+};
+class list{
+    Node* head;
+    Node* tail;
+public:
+  list()
+  {
+    head=tail=NULL;
+  }
+  void push_front( int val){
+       Node* newNode=new Node(val);
+       if(head==NULL){
+        head=tail=newNode;
+        return;
+       }
+       else{
+        newNode->next=head;
+        head=newNode;
+       }
+  }
+  void push_back(int val){
+    Node* newNode=new Node(val);
+    if(head==NULL){
+        head=tail=newNode;
+        return;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+  }
+  void print()
+  {
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+  }
+};
+int main()
+{
+    list ll;
+    ll.push_front(10); 
+    ll.push_front(20); 
+    ll.push_front(30); 
+    ll.push_front(40); 
+    ll.push_back(50);
+    ll.push_back(60);
+    ll.push_back(70);
+    ll.push_back(80);
+    ll.print();
+}  
