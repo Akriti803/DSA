eabfbd2cf7b8261bfde122e234414bf07ba45f92
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-08-15 14:54:38 +0530

    used slow and fast pointer approach  for this

---
+++ linked_list/middle_of_linked_list.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+class Node{
+public:
+       int data;
+       Node* next;
+       Node(int val){
+        data =val;
+        next=NULL;
+       }
+    
+
+};
+class list{
+    Node* head;
+    Node* tail;
+public:
+  list()
+  {
+    head=tail=NULL;
+  }
+  void push_back(int val){
+    Node* newNode=new Node(val);
+    if(head==NULL){
+        head=tail=newNode;
+        return;
+    }
+    else{
+        tail->next=newNode;
+        tail=newNode;
+    }
+  }
+  Node* middle(){
+    Node* slow=head;
+    Node* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    cout<<"the middle element is :"<<slow->data<<endl;
+  }
+  void print()
+  {
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+  }
+  
+};
+int main()
+{
+    list ll;
+    int n;
+    cout<<"enter the size of the list:";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cout<<"enter the value:"<<endl;
+        cin>>x;
+        ll.push_back(x);
+    }
+    ll.print();
+    ll.middle();
+}  
