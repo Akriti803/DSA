4847a5a1af9756923d2cfa66e925ebd017b84e05
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-08-16 23:21:22 +0530

    understood what to do if the user enters a space for the query

---
+++ hashing/mix_of_charcters.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter the string:";
+    getline(cin,s);
+    int q;
+    cout<<"enter the number of queries:"<<endl;
+    cin>>q;
+    cin.ignore();
+    int hash[256]={0};
+    for(int i=0;i<s.size();i++){
+        hash[s[i]]+=1;//isme the particular charcter apni ascii value pe jala jaayega kyun we have a mx size of 256 jisme we can have uppercase lowercase etc
+    }
+    while(q--){
+       char c;//example cahrch='d' ye aise aata hia isliye we dont write single quotes me c
+       cout<<"enter the charater:";
+       cin.get(c);//this will read the space
+       cin.ignore();
+       cout<<"the number of occurences of "<< c<<" is:"<<hash[c]<<endl;
+    }
+}
