fd22717757001792588dacecc46620818cbe3311
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-08-17 18:51:05 +0530

    practice question 2

--- hashing/appears_twice.cpp
+++ hashing/appears_twice.cpp
@@ -4,26 +4,21 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"enter the size of the array:";
-    cin>>n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
     int arr[n];
-    unordered_map<int,int>hashh;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    unordered_map<int,int> hashh;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
         hashh[arr[i]]++;
     }
-    int q;
-    cout<<"enter the number of queries:"<<endl;
-    cin>>q;
-    while(q--){
-        int num;
-        cout<<"enter the number:"<<endl;
-        cin>>num;
-        cout<<"the number of occurences is:"<<hashh[num]<<endl;
-    }
-    for(auto it:hashh){
-        if(it.second==2){
-            cout<<"the element which appears twice is:"<<it.first<<endl;
+    cout << "Elements appearing exactly twice are: ";
+    for(auto it : hashh)
+    {
+        if(it.second == 2)
+        {
+            cout << it.first << " ";
         }
     }
 }
