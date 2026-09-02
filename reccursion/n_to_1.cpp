414e15d0eae81fa37e8b8a36813cbf6cb67f5249
Akriti803 <akritisingh.cs25@bmsce.ac.in>
2026-09-02 12:57:50 +0530

    code for n to 1

--- reccursion/n_to_1.cpp
+++ reccursion/n_to_1.cpp
@@ -1,15 +1,17 @@
 #include<iostream>
 using namespace std;
-void print(int n){
-    if(n==1){
+void print(int n)
+{
+    if(n==0)
+    {
         return;
     }
-    cout<<n-1<<endl;
+    cout<<n<<" ";
     print(n-1);
 }
-int main(){
+int main()
+{
     int n;
     cin>>n;
     print(n);
-    return 0;
 }
